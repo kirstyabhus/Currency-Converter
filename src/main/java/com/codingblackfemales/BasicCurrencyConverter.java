5e594cad5d8279a9e07c333795731d06421c84e1
Kirsty Abhulimhen <kabhulimhen@gmail.com>
2023-08-04 19:29:51 +0100

    implement BasicCurrencyConverter & methods

--- src/main/java/com/codingblackfemales/BasicCurrencyConverter.java
+++ src/main/java/com/codingblackfemales/BasicCurrencyConverter.java
@@ -1,5 +1,21 @@
 package com.codingblackfemales;
 
-public class BasicCurrencyConverter {
+public class BasicCurrencyConverter implements CurrencyConverter {
+
+    // will return the converted amount (conversion from source to destination
+    // currency)
+    public double convertCurrency(String sourceCurrencyCose, String destinationCurrencyCode, double amount) {
+
+    }
+
+    // will return an array of available currency codes
+    public String[] getCurrencyCodes() {
+
+    }
+
+    // will return the exchange rate between the provided currencies.
+    public double getExchangeRate(String sourceCurrencyCode, String destinationCurrencyCode) {
+
+    }
 
 }
--- src/main/java/com/codingblackfemales/CurrencyConverter.java
+++ src/main/java/com/codingblackfemales/CurrencyConverter.java
@@ -2,4 +2,14 @@ package com.codingblackfemales;
 
 public interface CurrencyConverter {
 
+    // will return the converted amount (conversion from source to destination
+    // currency)
+    double convertCurrency(String sourceCurrencyCose, String destinationCurrencyCode, double amount);
+
+    // will return an array of available currency codes
+    String[] getCurrencyCodes();
+
+    // will return the exchange rate between the provided currencies.
+    double getExchangeRate(String sourceCurrencyCode, String destinationCurrencyCode);
+
 }
