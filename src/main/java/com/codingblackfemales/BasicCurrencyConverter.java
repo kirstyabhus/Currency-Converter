bdbd712e4a5a3d0a8bc7866de7c734bf54e3b69f
Kirsty Abhulimhen <kabhulimhen@gmail.com>
2023-08-04 19:55:05 +0100

    implement the getCurrencyCodes method #6

--- src/main/java/com/codingblackfemales/BasicCurrencyConverter.java
+++ src/main/java/com/codingblackfemales/BasicCurrencyConverter.java
@@ -1,21 +1,44 @@
 package com.codingblackfemales;
 
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class BasicCurrencyConverter implements CurrencyConverter {
 
     // will return the converted amount (conversion from source to destination
     // currency)
-    public double convertCurrency(String sourceCurrencyCose, String destinationCurrencyCode, double amount) {
-
-    }
+    /*
+     * public double convertCurrency(String sourceCurrencyCose, String
+     * destinationCurrencyCode, double amount) {
+     * 
+     * }
+     */
 
     // will return an array of available currency codes
     public String[] getCurrencyCodes() {
+        // create new instance of the CurrenciesGBP class (to access its methods)
+        CurrenciesGBP currenciesGBP = new CurrenciesGBP();
 
+        // get the exchange rates (hash map)
+        HashMap<String, Double> exchangeRates = currenciesGBP.getAllExchangeRates();
+
+        // create a new array, the size of the key set (to store the key codes)
+        String[] currencyCodes = new String[exchangeRates.size()];
+
+        // transverse through the set of key codes, adding each key to the array
+        int i = 0;
+        for (String code : exchangeRates.keySet()) {
+            currencyCodes[i] = code;
+        }
+
+        return currencyCodes;
     }
-
-    // will return the exchange rate between the provided currencies.
-    public double getExchangeRate(String sourceCurrencyCode, String destinationCurrencyCode) {
-
-    }
-
+    /*
+     * // will return the exchange rate between the provided currencies.
+     * public double getExchangeRate(String sourceCurrencyCode, String
+     * destinationCurrencyCode) {
+     * 
+     * }
+     */
 }
