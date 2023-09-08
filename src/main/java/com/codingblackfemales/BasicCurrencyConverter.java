16dad7861bfacdff937e5cbc5a8ddd5aa46a9c1c
Kirsty Abhulimhen <kabhulimhen@gmail.com>
2023-09-08 23:01:19 +0100

    add exception check

--- src/main/java/com/codingblackfemales/BasicCurrencyConverter.java
+++ src/main/java/com/codingblackfemales/BasicCurrencyConverter.java
@@ -3,10 +3,13 @@ package com.codingblackfemales;
 import java.lang.reflect.Array;
 import java.util.HashMap;
 import java.util.Set;
+
+import com.codingblackfemales.exceptions.currencyExceptions;
+
 import java.util.HashSet;
 
 public class BasicCurrencyConverter implements CurrencyConverter {
-    double errorValue = 0.0;
+    // double errorValue = 0.0;
     HashMap<String, Double> currenciesGBP;
 
     // add constuctor
@@ -17,6 +20,13 @@ public class BasicCurrencyConverter implements CurrencyConverter {
     // will return the converted amount (conversion from source to destination
     // currency)
     public double convertCurrency(String sourceCurrencyCode, String destinationCurrencyCode, double amount) {
+
+        currencyExceptions currencyExceptions = new currencyExceptions();
+        double exceptionValue = currencyExceptions.findException(sourceCurrencyCode, destinationCurrencyCode, amount);
+
+        if (exceptionValue == 0.0) {
+            return 0.0;
+        }
 
         // TODO add Try-catch with return in try
         // TODO try throws again
@@ -52,13 +62,13 @@ public class BasicCurrencyConverter implements CurrencyConverter {
     // will return the exchange rate between the provided currencies.
     public double getExchangeRate(String sourceCurrencyCode, String destinationCurrencyCode) {
         // create new instance of the CurrenciesGBP class (to access its methods)
-        CurrenciesGBP currenciesGBP = new CurrenciesGBP();
+        // CurrenciesGBP currenciesGBP = new CurrenciesGBP();
 
         // get the exchange rates (hash map)
-        HashMap<String, Double> exchangeRates = currenciesGBP.getAllExchangeRates();
+        // HashMap<String, Double> exchangeRates = currenciesGBP.getAllExchangeRates();
 
         // calculate exchange rate between currencies using GBP exchange rate
-        double exchangeRate = exchangeRates.get(destinationCurrencyCode) / exchangeRates.get(sourceCurrencyCode);
+        double exchangeRate = currenciesGBP.get(destinationCurrencyCode) / currenciesGBP.get(sourceCurrencyCode);
 
         // TODO handle exception for Invalid or missing source currency code.
         // TODO handle exception for Invalid or missing destination currency code.
