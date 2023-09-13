552026882ab60dc78e9ed666b02118d473e98dd0
Kirsty Abhulimhen <kabhulimhen@gmail.com>
2023-09-14 00:06:06 +0100

    get user input for source, destination & amount #8

--- src/main/java/com/codingblackfemales/Main.java
+++ src/main/java/com/codingblackfemales/Main.java
@@ -1,10 +1,32 @@
 package com.codingblackfemales;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        // create an object of Scanner
+        Scanner input = new Scanner(System.in);
+
+        // ask the user for source code input
+        System.out.println("Enter your source currency code: ");
+        // take the source code input from the user
+        String sourceCurrency = input.nextLine();
+
+        // ask the user for destination code input
+        System.out.println("Enter your destination currency code: ");
+        // take the destination code input from the user
+        String destinationCurrency = input.nextLine();
+
+        // ask the user for the amount to convert
+        System.out.println("Enter the amount of " + sourceCurrency + " to convert to " + destinationCurrency + ":");
+        // take the amount input from the user
+        double amount = input.nextDouble();
+
+        // close the scanner object
+        input.close();
 
         CurrenciesGBP currenciesGBP = new CurrenciesGBP();
 
@@ -14,7 +36,9 @@ public class Main {
         System.out.println(Arrays.toString(converter.getCurrencyCodes()));
 
         // output exchange rate
-        System.out.println(converter.getExchangeRate("GBP", "EUR"));
+        System.out.println(converter.getExchangeRate(sourceCurrency, destinationCurrency));
+
+        System.out.println(converter.convertCurrency(sourceCurrency, destinationCurrency, amount));
 
     }
 }
